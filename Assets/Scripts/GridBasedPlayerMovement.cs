6a24a20f82005b80f446a414c66a24c9fc0fc7a3
chos906 <80611197+chos906@users.noreply.github.com>
2022-11-06 17:41:00 -0500

    Map
    second draft of level 1

---
+++ Assets/Scripts/GridBasedPlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBasedPlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
