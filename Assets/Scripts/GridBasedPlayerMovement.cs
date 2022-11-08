532154b225624a078e5faf6be5e11f7e0824ab90
BlakeOneal <blakeoneal@ufl.edu>
2022-11-07 21:46:19 -0500

    Squashed commit of the following:
    commit 6a24a20f82005b80f446a414c66a24c9fc0fc7a3
    Author: chos906 <80611197+chos906@users.noreply.github.com>
    Date:   Sun Nov 6 17:41:00 2022 -0500
    
        Map
    
        second draft of level 1

--- Assets/Scripts/GridBasedPlayerMovement.cs
+++ Assets/Scripts/GridBasedPlayerMovement.cs
@@ -11,28 +11,26 @@ public class GridBasedPlayerMovement : MonoBehaviour
     float moveLimiter = 0.7f;
 
     public float runSpeed = 20.0f;
-
+    // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Gives a value between -1 and 1
-        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
-        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        if (horizontal != 0 && vertical != 0)
         {
-            // limit movement speed diagonally, so you move at 70% speed
             horizontal *= moveLimiter;
             vertical *= moveLimiter;
         }
-
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
